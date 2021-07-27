afde9e422080d7cb954ec882f7a9033bd6d31062
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2021-07-27 21:42:30 +0200

    api updates

--- New folder/main.js
+++ New folder/main.js
@@ -17,53 +17,74 @@
 // https://www.googleapis.com/youtube/v3/channels?part=contentDetails&channelId=UCmA-0j6DRVQWo4skl8Otkiw &key=AIzaSyDnDRQc-4KUg6uJFZK3FY2qHBdpB1KVqtY 
 // get channel logo -https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername=linustechtips&key=AIzaSyDnDRQc-4KUg6uJFZK3FY2qHBdpB1KVqtY
 
-const channels = ["linustechtips","comicstorian","shortcircut","hardwarecanucks","techquickie","motogp","engineeringexplained","crunchyroll","randomfrankp","carwow"]
-load_youtube()
+const channels = ["linustechtips","comicstorian","hardwarecanucks","techquickie","motogp","engineeringexplained","crunchyroll","randomfrankp"]
+// load_youtube()
+// load_github()
 async function load_github(){
-    let git_url = "https://api.spacexdata.com/v3/launches"
+    let git_url = "https://api.github.com/users/FrancoBester"
     const git_repsonse = await fetch(git_url)
     const git_data = await git_repsonse.json()
-
+    load_gitdata_html(git_data)
 }
 
-function load_gitdata(gitdata){
-
+function load_gitdata_html(githubdata){
+    document.getElementById("github").innerHTML=`
+    <p><a href="#Links" onclick=open_new_window("${githubdata.html_url.slice(7,40)}")>Github account information</a></p> 
+    <div class="background_api">
+    <img src="${githubdata.avatar_url}"/>
+    <p>Account name: ${githubdata.login}</p>
+    <p>Account type: ${githubdata.type}</p>
+    <p>Lastest upload: ${githubdata.updated_at.slice(0,10) + " " + githubdata.updated_at.slice(11,19)}</p>
+    <p>On Github since: ${githubdata.created_at.slice(0,10)}</p>
+    </div>
+    `
 }
 
 function load_youtube(){
     let base_youtube_url = "https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername="
     let end_youtube_url = "&key=AIzaSyDnDRQc-4KUg6uJFZK3FY2qHBdpB1KVqtY"
-    let channel_name = ""
-    // test(base_youtube_url+"linustechtips"+end_youtube_url)
-    // const test_data = await fetch(base_youtube_url+"linustechtips"+end_youtube_url)
     channels.forEach(element => {
         channel_name = element
         console.log(element)
-        const test_data = test(base_youtube_url+element+end_youtube_url)
-        // const test = await test_data.json
-        // console.log(test)
+        get_channel_image(base_youtube_url+element+end_youtube_url)
     });
 }
 
-async function test(url){
-    const data = await fetch(url)
-    const json_data = await data.json()
-
-    dat_test(json_data.items[0].snippet.thumbnails.medium.url)
-    // console.log(json_data.items[0].snippet.thumbnails.medium.url)
-    // document.getElementById("youtube").innerHTML = `
-    //     <img src="${json_data.items[0].snippet.thumbnails.medium.url}">
-    // `
-    // console.log(url)
-    // console.log(json_data.items[0].snippet.thumbnails.medium.url)
-    // let original_html = document.getElementById("youtube")
-    // original_html.innerHTML += '<img src="${json_data.items[0].snippet.thumbnails.medium.url}>"'
-    // let new_html = '<img src="${json_data.items[0].snippet.thumbnails.medium.url}>"';
-    // document.getElementById("youtube").innerHTML = new_html
-    // console.log(document.getElementById("youtube").innerHTML)
+async function get_channel_image(youtube_url){
+    const raw_data = await fetch(youtube_url)
+    const json_data = await raw_data.json()
+    load_youtube_html(json_data.items[0].snippet.thumbnails.medium.url)
 }
 
-function dat_test(info){
+function load_youtube_html(channel_info){
     let original_html = document.getElementById("youtube")
-    original_html.innerHTML += `<img src="${info}">`
+    original_html.innerHTML += `<img src="${channel_info}">`
+}
+
+async function load_spaceX(){
+    let base_space_url = "https://api.spacexdata.com/"
+    let launch_version = "v4"
+    let end_space_url = "/launches"
+    const spacex_repsonse = await fetch(base_space_url+launch_version+end_space_url)
+    const spcex_data = await spacex_repsonse.json()
+    load_spaceX_html
+
+}
+
+function load_spaceX_html(spacedata){
+    document.getElementById("spacex").innerHTML=`
+    <p> <a href='#Links' onclick=open_new_window("www.spacex.com/")>Latest SpaceX information</a></p>
+    <div class="background_api" style="padding-bottom:0.5rem">
+    <img src='https://www.spacex.com/static/images/backgrounds/starlink_11_dekstop.jpg' class="space_img"/>
+    <p>Fight number : ${spacedata.flight_number}</p>
+    <p>Mission name : ${spacedata.mission_name}</p>
+    <p>Current local launch date: ${spacedata.launch_date_local.slice(0,10) +" "+spacedata.launch_date_local.slice(11,19)} </p>
+    <p>Rocket id: ${spacedata.rocket.rocket_id}</p>
+    <p>Rocket name: ${spacedata.rocket.rocket_name}</p>
+    <p>Rocket type: ${spacedata.rocket.rocket_type}</p>
+    <p>Rocket manufactorer: ${spacedata.rocket.second_stage.payloads[0].manufacturer}</p>
+    <p>Rocket payload: ${spacedata.rocket.second_stage.payloads[0].payload_type}</p>
+    </div>
+    <div style="height:4.9rem"></div>
+    `
 }
