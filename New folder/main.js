4eb39e45090a3d8f73ee23183a110ecf11c75bc5
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2021-07-30 23:26:55 +0200

    improved api html and css

--- New folder/main.js
+++ New folder/main.js
@@ -29,14 +29,12 @@ async function load_github(){
 
 function load_gitdata_html(githubdata){
     document.getElementById("github").innerHTML=`
-    <p><a href="#Links" onclick=open_new_window("${githubdata.html_url.slice(7,40)}")>Github account information</a></p> 
-    <div class="background_api">
     <img src="${githubdata.avatar_url}"/>
-    <p>Account name: ${githubdata.login}</p>
-    <p>Account type: ${githubdata.type}</p>
-    <p>Lastest upload: ${githubdata.updated_at.slice(0,10) + " " + githubdata.updated_at.slice(11,19)}</p>
-    <p>On Github since: ${githubdata.created_at.slice(0,10)}</p>
-    </div>
+    <h4 class="github_text">Account link:  <a href=#Links onclick=open_new_window("${githubdata.html_url.slice(7,40)}") style="color: #e61251;">github-FrancoBester</a></h4> 
+    <h4 class="github_text">Account name: ${githubdata.login}</h4>
+    <h4 class="github_text">Account type: ${githubdata.type}</h4>
+    <h4 class="github_text">Lastest commit: ${githubdata.updated_at.slice(0,10)}</h4>
+    <h4 class="github_text">On Github since: ${githubdata.created_at.slice(0,10)}</h4>
     `
 }
 
@@ -45,7 +43,6 @@ function load_youtube(){
     let end_youtube_url = "&key=AIzaSyDnDRQc-4KUg6uJFZK3FY2qHBdpB1KVqtY"
     channels.forEach(element => {
         channel_name = element
-        // console.log(element)
         get_channel_image(base_youtube_url+element+end_youtube_url)
     });
 }
@@ -53,10 +50,16 @@ function load_youtube(){
 async function get_channel_image(youtube_url){
     const raw_data = await fetch(youtube_url)
     const json_data = await raw_data.json()
-    load_youtube_html(json_data.items[0].snippet.thumbnails.medium.url)
+    load_youtube_html(json_data.items[0].snippet)
 }
 
 function load_youtube_html(channel_info){
     let original_html = document.getElementById("youtube")
-    original_html.innerHTML += `<img src="${channel_info}" class='youtube_coin'>`
+    let youtube_link = "www.youtube.com/"+(channel_info.title).replaceAll(" ","")
+    console.log(youtube_link)
+    original_html.innerHTML += `<a href=#Links onclick=open_new_window("${youtube_link}")><img src="${channel_info.thumbnails.high.url}" class='youtube_coin'> </a>`
 }
+
+function open_new_window(URL){//function used to open other webpages in a new browser window
+    window.open("https://"+URL);
+};
